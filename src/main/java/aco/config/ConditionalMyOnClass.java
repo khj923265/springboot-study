c03da9c32dd54c135c06b9ba1be292cbc8dec791
khj923265 <khj923265@naver.com>
2023-03-21 22:10:00 +0900

    update: @ConditionalOnMissingBean 추가

--- src/main/java/aco/config/ConditionalMyOnClass.java
+++ src/main/java/aco/config/ConditionalMyOnClass.java
@@ -9,7 +9,7 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE, ElementType.METHOD})
-@Conditional(MyOnClassConditional.class)
+@Conditional(MyOnClassConditional.class) // spring 4.0 부터 나온 springframework 에 있는 기능
 public @interface ConditionalMyOnClass {
     String value();
 }
--- src/main/java/aco/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/aco/config/autoconfig/TomcatWebServerConfig.java
@@ -2,6 +2,7 @@ package aco.config.autoconfig;
 
 import aco.config.ConditionalMyOnClass;
 import aco.config.MyAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.Bean;
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
     @Bean("tomcatWebServerFactory")
+    @ConditionalOnMissingBean // 메서드레벨에서 해당 빈이 등록이 되어있으면 무시하고 등록되지않으면 아래 빈을 등록시킴
     public ServletWebServerFactory servletWebServerFactory() {
         return new TomcatServletWebServerFactory();
     }
